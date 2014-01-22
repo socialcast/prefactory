a15c2a51093a7a0b014f530be620661c340f05bd
Sean Walbran <swalbran@socialcast.com>
2014-01-22 15:08:21 -0600

    add spec coverage for with_disposable_transaction

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
+describe "WithDisposableTransactionExtension#with_disposable_transaction" do
+  let!(:blog) { Blog.create! :title => 'foo' }
+  it "runs a transaction and rolls it back" do
+    previous_open_transaction_count = Blog.connection.open_transactions
+    Blog.with_disposable_transaction do
+      blog.update_attributes! :title => 'bar'
+      blog.reload.title.should == 'bar'
+      Blog.connection.open_transactions.should == previous_open_transaction_count + 1
+    end
+    blog.reload.title.should == 'foo'
+    Blog.connection.open_transactions.should == previous_open_transaction_count
+  end
+  it "preserves the return value" do
+    result = Blog.with_disposable_transaction do
+      1
+    end
+    result.should == 1
+  end
+  it "raises any StandardError occuring in the block" do
+    expect do
+      Blog.with_disposable_transaction do
+        raise "Example Error"
+      end
+    end.to raise_error("Example Error")
+  end
+end
+
 describe "Transactional wrapping" do
   it "wraps describe blocks in a savepoint transaction" do
     ActiveRecord::Base.connection.current_transaction.should be_a ActiveRecord::ConnectionAdapters::SavepointTransaction
